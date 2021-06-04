3e21fa907a56b41421194777ffe951d69b7d719a
caolainia <xzha4350@gmail.com>
2021-06-04 19:56:07 +0930

    migration

--- app/Http/Controllers/Api/ProductApiController.php
+++ app/Http/Controllers/Api/ProductApiController.php
@@ -13,7 +13,11 @@ class ProductApiController extends Controller
         return Product::all();
     }
 
-    public function create() {
+    public function show(Product $product) {
+        return Product::find($product);
+    }
+
+    public function create(Product $product) {
         request()->validate([
             'product_name' => 'required',
             'original_price' => 'required',
@@ -25,7 +29,7 @@ class ProductApiController extends Controller
         ]);
     }
 
-    public function update(Post $product) {
+    public function update(Product $product) {
         request()->validate([
             'product_name' => 'required',
             'original_price' => 'required',
@@ -41,7 +45,7 @@ class ProductApiController extends Controller
         ];
     }
 
-    public function delete(Post $product) {
+    public function delete(Product $product) {
         $success = $product->delete();
 
         return [
--- database/migrations/2021_05_12_085952_create_products_table.php
+++ database/migrations/2021_05_12_085952_create_products_table.php
@@ -15,9 +15,20 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('product_name');
-            $table->string('unit')->comment('the unit of product, ie. pc(piece), pair, box, etc.')->default('pc');
+            // SKU formula: (different color must have diff sku)
+            // J{S(hoes), C(lothes), P(ants), B(ag)}
+            // -{Category Chain: Gender(1d)+}
+            // -{1000-id+id%13}
+            $table->char('sku', 13)->unique()->nullable();
+            $table->string('product_name', 200);
+            $table->integer('gender')->comment('suitable gender, 0:f, 1:m, 2:neutral, 3:kids')->default(2);
+            $table->string('brand', 50)->default('Adidas');
+            $table->string('color', 50);
+            $table->integer('category')->default(0)->comment("deepest category id");
             $table->integer('original_price')->comment('in AUD')->default(0);
+            $table->integer('current_price')->comment('in AUD')->default(0);
+            $table->integer('amt_sold')->default(0);
+            $table->integer('amt_viewed')->default(0);
             $table->text('product_description')->nullable();
             $table->text("product_thumbnail")->comment("thumbnail image url")->nullable();
             $table->timestamps();

37eafa05f7ea5e800cc960d2c46c4a4836945d33
Ubuntu <ubuntu@ip-172-31-22-46.ap-southeast-2.compute.internal>
2021-06-13 10:00:09 +0000

    staging

--- public/js/app.js
+++ public/js/app.js
@@ -1947,8 +1947,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__.default({
   broadcaster: 'pusher',
-  key: "d5774d5ac2f4d9f66e63",
-  cluster: "ap4",
+  key: "",
+  cluster: "mt1",
   forceTLS: true
 });
 
